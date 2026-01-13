4434a46e91e813f88f7a3f4b4955c040c7ef0395
Michiel De Backer <michiel1.debacker@gmail.com>
2026-01-13 15:50:40 +0100

    Finished Ben laser

---
+++ NewBananaWeapons/Behaviours/BenjaminLaserWeapon.cs
@@ -0,0 +1,40 @@
+﻿using NewBananaWeapons;
+using System.Collections;
+using UnityEngine;
+
+public class BenjaminLaserWeapon : MonoBehaviour
+{
+    public GameObject chargingLaser;
+    public GameObject laser;
+
+    Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        StartCoroutine(ShaderManager.ApplyShaderToGameObject(laser));
+    }
+    void Update()
+    { 
+        if (Banana_WeaponsPlugin.cooldowns.ContainsKey(gameObject))
+        {
+            chargingLaser.SetActive(false);
+            anim.SetBool("Activating", false);
+            return;
+        }
+
+        chargingLaser.SetActive(true);
+
+        if (InputManager.instance.InputSource.Fire1.WasPerformedThisFrame)
+        {
+            anim.SetBool("Activating", true);
+        }
+    }
+
+    public void FireLaser()
+    {
+        Instantiate(laser, CameraController.Instance.transform.position, 
+            CameraController.Instance.transform.rotation).transform.Rotate(90, 0, 0);
+        Banana_WeaponsPlugin.cooldowns.Add(gameObject, 25);
+    }
+}
